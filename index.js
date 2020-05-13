e437ba0c208d0c0524c299082471e948d1a1bb05
karthikkornalies <karthik.it2004@gmail.com>
2020-05-13 12:02:52 -0500

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter TEST</h1>`;
+appDiv.innerHTML = `<h1>JS TEST</h1>`;
 
 
 const myObj = { a : 1, a : 10, b : '2', c : 3};
@@ -23,3 +23,29 @@ function pickBy(arr, myFn) {
 }
 
 pickBy(myObj, myFn);
+
+
+let names = ['a1', 'a2', 'a3', 'a4'];
+  console.log(names);
+names.forEach(elementIterate);
+
+function elementIterate(v, i) {
+  console.log(v + ' ' + i);
+}
+/*const unzip = arr =>
+  arr.reduce(
+    (accum, currentVal) => (currentVal.forEach(v,i) => accum[i].push(v)), accum),
+    Array.from({
+      length : Math.max(...arr.map(x => x.length))
+    }).map(x => [])
+  );*/
+
+  const hideAll = (elementArr) => {
+   elementArr.forEach(e => (e.style.display = 'none'));
+  }
+
+  //hideAll( document.querySelectorAll('img'));
+
+  const t = names.map (n => n + '*');
+  console.log(' ## MAP ' + t);
+
